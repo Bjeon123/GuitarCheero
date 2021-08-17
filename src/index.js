97cd179cf950521d53bb287c49f6b4f54d44f914
Samuel Jeon <samueljeon99@gmail.com>
2021-08-17 00:25:42 -0400

    fixed file structure

--- src/index.js
+++ src/index.js
@@ -4,16 +4,4 @@ import { renderHome} from './screens/homeScreen';
 
 document.addEventListener("DOMContentLoaded",function(){
     renderHome();
-    // const canvas = document.getElementById("game-canvas")
-    // const ctx = canvas.getContext("2d");
-    // const game = new Game();
-    // const musicPlayer= new MusicPlayer();
-    // window.addEventListener("keydown",function(e){
-    //     if(e.key==="m"){
-    //         setTimeout(function(){
-    //         musicPlayer.play()},3000);
-    //         const newGameView = new GameView(game, ctx);
-    //     }
-    // })
- 
 })
